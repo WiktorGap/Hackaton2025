d0cd4e05e5884b8aea3272842b963b3dcc898522
Disu111 <120790215+Disu111@users.noreply.github.com>
2025-10-19 00:27:21 +0200

    Delete app directory

--- app/main/routes.py
+++
@@ -1,9 +0,0 @@
-from . import main
-from datetime import datetime , timezone 
-from flask import render_template, session, redirect, url_for, make_response, request, flash, current_app
-
-
-@main.route('/',methods=['GET','POST'])
-def index():
-    current_time = datetime.now(timezone.utc)
-    return render_template('base.html', current_time=current_time)
